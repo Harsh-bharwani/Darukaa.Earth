9ea6bbba4cac410f6256a371841b8194673855da
Harsh Bharwani <harshbharwani1234@gmail.com>
2026-09-18 18:51:22 +0530

    chore: implement centralized axios instance with automated jwt interceptor

---
+++ frontend/src/utils/api.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+// Establishes a unified network anchor pointing to our local FastAPI server instance
+const api = axios.create({
+  baseURL: "http://127.0.0.1:8000",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+// Outgoing Request Interceptor: Automatically attaches security tokens to protected map endpoints
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+
+export default api;
